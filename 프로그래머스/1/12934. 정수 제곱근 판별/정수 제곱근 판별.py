90878337450edf48a1aedf2f4494eca7e5ace9c1
KoHo <onestar5123@naver.com>
2024-09-30 12:49:13 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,6 +1,8 @@
 def solution(n):
-    for x in range(1, n + 1):
-        if x ** 2 == n:
-            return (x + 1) ** 2
+#     for x in range(1, n + 1):
+#         if x ** 2 == n:
+#             return (x + 1) ** 2
         
-    return -1
+#     return -1
+    
+    return (n ** (1 / 2) + 1) ** 2 if n ** (1 / 2) % 1 == 0 else -1
