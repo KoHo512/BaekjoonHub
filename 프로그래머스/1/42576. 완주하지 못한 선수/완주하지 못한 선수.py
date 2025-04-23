ba3238137ad0d8c16a92e56a009917c2a1cb9526
ywJung <sesac.yw@gmail.com>
2025-04-23 15:28:56 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 35.74 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+def solution(participant, completion):
+#     for name in completion:
+#         if name in participant:
+#             participant.remove(name)
+            
+#     return participant[0]
+
+    participant_counter = Counter(participant)
+    completion_counter = Counter(completion)
+    
+    for person in participant:
+        if participant_counter.get(person) != completion_counter.get(person):
+            return person
+        
