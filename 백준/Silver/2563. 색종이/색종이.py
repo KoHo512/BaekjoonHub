a57b8b7e9b5a3952b3b907588861e4a9e1406d06
ywJung <sesac.yw@gmail.com>
2025-04-23 23:43:12 +0900

    [Silver V] Title: 색종이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -4,14 +4,13 @@ input = sys.stdin.readline
 
 n = int(input())
 
-papers = [list(map(int, input().split())) for _ in range(n)]
+paper = [[0] * 100 for _ in range(100)]
 
-white_paper = [[0] * 100 for _ in range(100)]
+for _ in range(n):
+    x, y = map(int, input().split())
 
-for paper in papers:
-    for i in range(paper[0], paper[0] + 10):
-        for j in range(paper[1], paper[1] + 10):
-            white_paper[i][j] = 1
+    for i in range(x, x + 10):
+        for j in range(y, y + 10):
+            paper[i][j] = 1
 
-
-print(sum([sum(line) for line in white_paper]))
+print(sum(map(sum, paper)))
