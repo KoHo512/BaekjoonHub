9e9d82ce94ca7d6abcf887a43c0a0b21d0e7b38e
ywJung <sesac.yw@gmail.com>
2025-05-03 16:13:11 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.45 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -12,18 +12,24 @@ def solution(maps):
                 nx, ny = x + dx[i], y + dy[i]
                 
                 if nx == n - 1 and ny == m - 1:
-                    return distance + 1
+                    nonlocal answer
+                    answer = min(answer, distance + 1)
+                    return
                 
                 if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and maps[nx][ny] == 1:
                     visited[nx][ny] = True
                     queue.append((nx, ny, distance + 1))
                     
-        return -1
-                    
     n, m = len(maps), len(maps[0])
     x, y = 0, 0
     dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
     visited = [[False] * m for _ in range(n)]
+    answer = float("inf")
     
     visited[x][y] = True
-    return bfs(x, y)
+    bfs(x, y)
+    
+    if answer == float("inf"):
+        return -1
+    
+    return answer
