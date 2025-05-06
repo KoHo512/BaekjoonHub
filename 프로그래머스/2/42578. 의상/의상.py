fba375d64d8d62ed509dfd066d269bb2dae33cd8
ywJung <sesac.yw@gmail.com>
2025-05-06 17:58:10 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,16 +1,14 @@
 def solution(clothes):
     closet = {}
     
-    for cloth in clothes:
-        if cloth[1] in closet:
-            closet[cloth[1]].append(cloth[0])
+    for c, t in clothes:
+        if t in closet:
+            closet[t].append(c)
         else:
-            closet[cloth[1]] = [cloth[0]]
-            
-    num_types = list(map(len, closet.values()))
+            closet[t] = [c]
     
     answer = 1
-    for type_num in num_types:
-        answer *= type_num + 1
+    for items in closet.values():
+        answer *= len(items) + 1
         
     return answer - 1
