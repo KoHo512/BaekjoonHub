b726e447b359ebdd3f3a05579a5c36c6a4978bdc
ywJung <sesac.yw@gmail.com>
2025-05-08 12:46:21 +0900

    [Silver II] Title: 로또, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+while True:
+    k, *s = map(int, input().rsplit())
+
+    if k == 0:
+        break
+
+    def combinations(current, start):
+        if len(current) == 6:
+            print(*current)
+            return
+        
+        for i in range(start, k):
+            current.append(s[i])
+            combinations(current, i + 1)
+            current.pop()
+
+    combinations([], 0)
+
+    print()
