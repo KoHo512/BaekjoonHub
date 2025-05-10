cd6ff049c47ec70e5c18c84ade5e72512d649186
ywJung <sesac.yw@gmail.com>
2025-05-10 18:02:54 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 9.07 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -0,0 +1,23 @@
+def solution(keyinput, board):
+    m, n = board
+    
+    x, y = 0, 0
+    dx, dy = [0, 0, -1, 1], [1, -1, 0, 0]
+    
+    for key in keyinput:
+        # if key == "up":
+        #     i = 0
+        # elif key == "down":
+        #     i = 1
+        # elif key == "left":
+        #     i = 2
+        # else:
+        #     i = 3
+        i = { "up": 0, "down": 1, "left": 2, "right": 3 }.get(key)
+        
+        nx, ny = x + dx[i], y + dy[i]
+        
+        if - m / 2 <= nx <= m / 2 and - n / 2 <= ny <= n / 2:
+            x, y = nx, ny
+        
+    return [x, y]
