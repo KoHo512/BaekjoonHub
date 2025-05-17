10b593313bc9e1d2a563802479d1528c402f9767
ywJung <sesac.yw@gmail.com>
2025-05-18 03:02:28 +0900

    [level 2] Title: 전화번호 목록, Time: 327.20 ms, Memory: 98.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -6,7 +6,6 @@ class Solution {
         Arrays.sort(phone_book);
         
         for (int i = 0; i < n - 1; i++) {
-            if (phone_book[i].startsWith(phone_book[i + 1])) return false;
             if (phone_book[i + 1].startsWith(phone_book[i])) return false;
         }
         
