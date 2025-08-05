c1c4e30a6b800d7a5c6fe0bdddde740e0939a57d
ywJung <sesac.yw@gmail.com>
2025-08-05 14:31:01 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.65 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,26 +1,19 @@
-from collections import deque
-
 def solution(board, moves):
-    m, n = len(board[0]), len(board)
-    
-    queue = [deque() for _ in range(m)]
-    basket = []
     answer = 0
-    
-    for x in range(m):
-        for y in range(n-1, -1, -1):
-            if board[y][x] > 0:
-                queue[x].append(board[y][x])
+    basket = []
     
     for move in moves:
         move -= 1
-        if queue[move]:
-            doll = queue[move].pop()
-
-            if basket and basket[-1] == doll:
-                answer += 2
-                basket.pop()
-            else:
-                basket.append(doll)
-    
-    return answer 
+        
+        for line in board:
+            if line[move] > 0:
+                if basket and basket[-1] == line[move]:
+                    answer += 2
+                    basket.pop()
+                else:
+                    basket.append(line[move])
+                
+                line[move] = 0
+                break            
+            
+    return answer
