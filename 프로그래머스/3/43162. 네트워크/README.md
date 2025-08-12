70d33e2fb3ebe9ddb2e53819aff06a44810e0593
ywJung <sesac.yw@gmail.com>
2025-08-12 17:59:29 +0900

    [level 3] Title: 네트워크, Time: 1.32 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
 
 ### 성능 요약
 
-메모리: 88.4 MB, 시간: 0.31 ms
+메모리: 9.21 MB, 시간: 1.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 18일 11:56:11
+2025년 08월 12일 17:59:25
 
 ### 문제 설명
 
---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,20 @@
+def solution(n, computers):
+    network = 0
+    visited = [False] * n
+    
+    def dfs(node):
+        visited[node] = True
+            
+        for n_node in range(n):
+            if node != n_node and computers[node][n_node] == 1 and not visited[n_node]:
+                dfs(n_node)
+                    
+    for i in range(n):
+        if not visited[i]:
+            dfs(i)
+            network += 1
+                    
+    return network
+
+                
+                
