58d0b35e29701c26f5e1cd32e7df401630b7dc34
ywJung <sesac.yw@gmail.com>
2025-08-19 14:11:55 +0900

    [level 3] Title: 베스트앨범, Time: 0.13 ms, Memory: 9.02 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,17 +1,14 @@
 def solution(genres, plays):
     playlist = {}
     genre_plays = {}
-    
-    for genre in set(genres):
-        playlist[genre] = {}
         
-    for i in range(len(plays)):
-        playlist[genres[i]][i] = plays[i]
-        genre_plays[genres[i]] = genre_plays.get(genres[i], 0) + plays[i]
+    for i, genre in enumerate(genres):
+        playlist.setdefault(genre, {})[i] = plays[i]
+        genre_plays[genre] = genre_plays.get(genre, 0) + plays[i]
         
     answer = []
     for genre, _ in sorted(genre_plays.items(), key=lambda x: -x[1]):
-        sorted_song = sorted(playlist[genre].items(), key=lambda x: -x[1])
+        sorted_song = sorted(playlist[genre].items(), key=lambda x: (-x[1], x[0]))
         answer.extend([song for song, _ in sorted_song[:2]])
         
     return answer
