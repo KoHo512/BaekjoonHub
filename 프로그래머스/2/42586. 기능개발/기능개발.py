f5d4a2d0a5d33d225e9d98c0765943481d893180
ywJung <sesac.yw@gmail.com>
2025-08-21 14:37:36 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,15 +1,12 @@
 import math
 
 def solution(progresses, speeds):
-    days = []
-    
-    for progress, speed in zip(progresses, speeds):
-        days.append(math.ceil((100 - progress) / speed))
-    
     answer = []
     max_day, total = 0, 1
     
-    for day in days:
+    for progress, speed in zip(progresses, speeds):
+        day = math.ceil((100 - progress) / speed)
+
         if day <= max_day:
             total += 1
         else:
