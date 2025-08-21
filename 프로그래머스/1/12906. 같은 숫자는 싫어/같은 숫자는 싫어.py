53fcd19664211815db583c0ee7ea19ea717d25dd
ywJung <sesac.yw@gmail.com>
2025-08-21 13:28:47 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 69.13 ms, Memory: 27.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    answer = []
+    
+    for num in arr:
+        if answer and num == answer[-1]:
+            continue
+        answer.append(num)
+        
+    return answer
