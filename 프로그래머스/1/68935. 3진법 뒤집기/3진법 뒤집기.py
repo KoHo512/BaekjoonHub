f5bd54672e9f9db31fc777424667353ef89334c7
ywJung <sesac.yw@gmail.com>
2025-08-25 17:07:37 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,14 +1,8 @@
 def solution(n):
-    answer = 0
     n3 = []
     
     while n:
         n3.append(n % 3)
         n //= 3
         
-    for i, num in enumerate(n3[::-1]):
-        answer += num * 3**i
-        
-    return answer
-        
-        
+    return int(''.join(map(str, n3)), 3)
