4666745f608290a5157d5426d6583dd969d9cc73
ywJung <sesac.yw@gmail.com>
2026-01-20 14:24:26 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 3.48 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -5,9 +5,9 @@ def solution(s):
     for i in range(len(s)):
         if s[i] in alpha.keys():
             answer.append(i - alpha[s[i]])
-            alpha[s[i]] = i
         else:
             answer.append(-1)
-            alpha[s[i]] = i
+            
+        alpha[s[i]] = i
             
     return answer
