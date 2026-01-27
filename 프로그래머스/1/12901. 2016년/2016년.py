a636ce3c67f1f29dbf65faa33140516e4d7157e9
ywJung <sesac.yw@gmail.com>
2026-01-27 18:12:22 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,14 +1,8 @@
 def solution(a, b):
-    date = 0
-    days = { 1:"FRI", 2:"SAT", 3:"SUN", 4:"MON", 5:"TUE", 6:"WED", 0:"THU" }
-    months = { 1:31, 2:29, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31 }
+    days = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"]
+    months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     
-    for i in range(1, a):
-        date += months[i]
-        
-    date += b
-    
-    return days[date%7]
+    return days[(sum(months[:a - 1]) + b) % 7]
     
     
     
